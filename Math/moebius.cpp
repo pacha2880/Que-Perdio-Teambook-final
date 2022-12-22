32e2e717076e31c7965dac5e2a04a124ab26c262
pacha2880 <salguero_ro@hotmail.com>
2022-12-21 22:42:16 -0400

    fix: mobiius

--- Math/moebius.cpp
+++ Math/moebius.cpp
@@ -1,5 +1,5 @@
 //f(n)=sum(d|n,g(d))=>g(n)=sum(d|n,f(d)*mu(n/d))
-//f(n)=sum(i->inf,f(i*n)*mu(i));f(n)=#f(a)->n;g(n)=#f(a)->xn
+//f(n)=sum(i->inf,g(i*n)*mu(i));f(n)=#f(a)->n;g(n)=#f(a)->xn
 int mu[tam], is_prime [tam];
 fore(i, 0, tam) mu[i]=is_prime[i]=1;
 fore(i, 2, tam) if(is_prime[i]) {
